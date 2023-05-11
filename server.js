1d357800f520784e9a41c2d2314e3d7483f12cbd
krisine <kristine.nyaga@student.moringaschool.com>
2023-05-11 15:40:52 +0300

    setup basic express server

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+  res.json({
+    hi:"welcome to js 2FA app"
+  })
+})
+
+app.listen(PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
